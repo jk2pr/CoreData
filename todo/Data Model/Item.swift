08ea23fad454efab6b4ea07354f09c975ac40f65
jitendra <jk2praj@gmail.com>
2018-08-05 09:16:23 +0530

    removed unusual comments

---
+++ todo/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Jitendra on 05/08/18.
+//  Copyright © 2018 Jitendra. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title:String=""
+    var done:Bool=false
+}
