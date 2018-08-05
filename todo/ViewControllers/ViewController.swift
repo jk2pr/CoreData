5739ad8a30ecbd74191e2e37636a8d359c2fbf7a
jitendra <jk2praj@gmail.com>
2018-08-05 10:02:12 +0530

    Added core data

--- todo/ViewControllers/ViewController.swift
+++ todo/ViewControllers/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
