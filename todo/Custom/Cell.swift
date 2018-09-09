3d765c4147566c5024e88fe452eafd187c00d477
jitendra <jk2praj@gmail.com>
2018-09-09 07:01:44 +0530

    added tableview custom xib

---
+++ todo/Custom/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  Todo
+//
+//  Created by Jitendra on 08/09/18.
+//  Copyright © 2018 Jitendra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Cell:UITableViewCell  {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- todo/ViewControllers/ViewController.swift
+++ todo/ViewControllers/ViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var data=[String]()
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 125
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath) as! Cell
+        cell.label.text=data[indexPath.row]
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableV: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        data.append("Jitendra")
+        data.append("Arvind")
+        data.append("Amit")
+        data.append("Suresh")
+        let nib = UINib(nibName: "cell", bundle: nil)
+       self.tableV.register(nib, forCellReuseIdentifier: "Cell")
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
